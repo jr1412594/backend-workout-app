4f5e00da45274e5b3879a65bf1529da6a0806a2f
jason1110 <Jason.easterly11@gmail.com>
2020-10-21 18:00:41 -0600

    adding create feature to program

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -10,4 +10,13 @@ class ProgramsController < ApplicationController
 
         render json: @program
     end
+
+    def create
+        @new_program = Program.create(
+            user_id: params[:user_id],
+            workout_id: params[:workout_id]
+        )
+        redirect_to "http://localhost:3000/user.html?id=#{@new_program.user_id}"
+    end
+
 end
